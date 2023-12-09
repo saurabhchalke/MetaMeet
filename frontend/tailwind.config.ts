e023313e5e3d1a5c3153fcff0c282323dc79b9cc
Saurabh Chalke <saurabhchalke@gmail.com>
2023-12-09 22:08:16 +0530

    Add config files

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,50 @@
-import type { Config } from 'tailwindcss'
+const colors = require("tailwindcss/colors")
+const { nextui } = require("@nextui-org/react")
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/api/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: {
+          DEFAULT: colors.purple[600],
+          ...colors.purple,
+        },
+        secondary: colors.yellow,
+        "section-darkest": "#0A0A0A",
+        "section-darker": "#141414",
+        "section-dark": "#1F1F1F",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      defaultTheme: "dark",
+      addCommonColors: true,
+    }),
+  ],
+  safelist: [
+    {
+      pattern: /justify-.+/,
+    },
+    {
+      pattern: /space-[xy]-\d+/,
+    },
+    {
+      pattern: /text-(neutral|primary|gray)-\d+/,
+    },
+  ],
 }
-export default config
