cb515410dc7f05f32a08c2d8dd4d5e704acb63e5
Saurabh Chalke <saurabhchalke@gmail.com>
2023-12-09 19:36:28 +0530

    Update go dependencies

--- backend/go.mod
+++ backend/go.mod
@@ -1,18 +1,70 @@
-module backend
+module github.com/darmiel/perplex
 
 go 1.21.4
 
 require (
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/aws/aws-sdk-go v1.48.3
+	github.com/go-playground/validator/v10 v10.15.5
+	github.com/gofiber/fiber/v2 v2.50.0
+	github.com/ralf-life/gofiber-firebaseauth v0.0.0-20230605133945-0096650c1e87
+	go.uber.org/zap v1.26.0
+	google.golang.org/api v0.147.0
+	gorm.io/driver/postgres v1.5.3
+	gorm.io/driver/sqlite v1.5.4
+	gorm.io/gorm v1.25.5
+)
+
+require (
+	cloud.google.com/go v0.110.8 // indirect
+	cloud.google.com/go/compute v1.23.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/firestore v1.13.0 // indirect
+	cloud.google.com/go/iam v1.1.2 // indirect
+	cloud.google.com/go/longrunning v0.5.1 // indirect
+	cloud.google.com/go/storage v1.33.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.51.0 // indirect
-	github.com/google/uuid v1.4.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.1 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20231002182017-d307bd883b97 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231002182017-d307bd883b97 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231009173412-8bfb1ae86b6c // indirect
+	google.golang.org/grpc v1.58.2 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,208 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	firebase "firebase.google.com/go"
+	"github.com/darmiel/perplex/api/handlers"
+	"github.com/darmiel/perplex/api/routes"
+	"github.com/darmiel/perplex/api/services"
+	"github.com/darmiel/perplex/pkg/model"
+	"github.com/darmiel/perplex/pkg/util"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	gofiberfirebaseauth "github.com/ralf-life/gofiber-firebaseauth"
+	"go.uber.org/zap"
+	"google.golang.org/api/option"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+	sugar := logger.Sugar()
 
-	// Setup a simple route
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	// firebase auth
+	fbApp, err := firebase.NewApp(context.TODO(), nil, option.WithCredentialsFile("config/firebase.json"))
+	if err != nil {
+		sugar.With(err).Fatalln("cannot create firebase app")
+		return
+	}
+
+	// database setup
+	var db *gorm.DB
+	if sqlitePath, ok := os.LookupEnv("SQLITE_PATH"); ok {
+		db, err = gorm.Open(sqlite.Open(sqlitePath))
+	} else if postgresDSN, ok := os.LookupEnv("POSTGRES_DSN"); ok {
+		db, err = gorm.Open(postgres.Open(postgresDSN))
+	} else {
+		sugar.Fatalln("no database specified")
+		return
+	}
+	if err != nil {
+		sugar.With(err).Fatalln("cannot open database")
+		return
+	}
+	if err = db.AutoMigrate(
+		new(model.User),
+		new(model.Comment),
+		new(model.Topic),
+		new(model.Meeting),
+		new(model.Project),
+		new(model.Priority),
+		new(model.Action),
+		new(model.Tag),
+		new(model.Notification),
+		new(model.ProjectFile),
+	); err != nil {
+		sugar.With(err).Fatalln("cannot migrate user")
+		return
+	}
+
+	// api
+	app := fiber.New(fiber.Config{
+		AppName:           "perplex-api",
+		StreamRequestBody: true,
+	})
+	app.Use(cors.New())
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Welcome to the perplex api! https://github.com/darmiel/perplex")
 	})
 
-	// Start the server on port 3000
-	app.Listen(":3000")
+	app.Use(gofiberfirebaseauth.New(fbApp, gofiberfirebaseauth.Config{
+		TokenExtractor: gofiberfirebaseauth.NewHeaderExtractor("Bearer "),
+	}))
+
+	s3Service, err := services.NewS3Service()
+	if err != nil {
+		sugar.With(err).Fatalln("cannot create s3 service")
+		return
+	}
+
+	projectService := services.NewProjectService(db)
+	meetingService := services.NewMeetingService(db)
+	topicService := services.NewTopicService(db, projectService)
+	commentService := services.NewCommentService(db, topicService)
+	userService := services.NewUserService(db, projectService, meetingService)
+	actionService := services.NewActionService(db, projectService)
+
+	// user middleware
+	// check if user is already registered in database
+	// if not, create user with username from email address
+	// and set username/email in ctx.Locals("user_id") for logging
+	app.Use(func(ctx *fiber.Ctx) error {
+		u := ctx.Locals("user").(gofiberfirebaseauth.User)
+		// check if user is already registered
+		if name, err := userService.GetName(u.UserID); err != nil {
+			// extract username from email
+			username := u.Email[:strings.Index(u.Email, "@")]
+			sugar.Infof("user %s is not registered yet, creating user with name %s", u.UserID, username)
+			// create user
+			if err = userService.ChangeName(u.UserID, username); err != nil {
+				ctx.Locals("friendly_user", u.Email)
+				sugar.Warnf("cannot create user %s: %v", u.UserID, err)
+			}
+			ctx.Locals("friendly_user", username)
+		} else {
+			ctx.Locals("friendly_user", name)
+		}
+		return ctx.Next()
+	})
+
+	// fiber logging middleware
+	app.Use(func(ctx *fiber.Ctx) error {
+		friendlyUser, ok := ctx.Locals("friendly_user").(string)
+		if !ok {
+			friendlyUser = "unknown"
+		}
+		err := ctx.Next()
+
+		sugar.Info(" ")
+		sugar.Debugf("[%s] %s %s -> (pending)", friendlyUser, ctx.Method(), ctx.Path())
+		if err != nil {
+			sugar.Infof("error: %v", err)
+			sugar.Infof("method: %s", ctx.Method())
+			sugar.Infof("path: %s", ctx.Path())
+			sugar.Infof("status: %d", ctx.Response().StatusCode())
+		} else {
+			sugar.Infof("[%s] %s %s -> %d", friendlyUser, ctx.Method(), ctx.Path(), ctx.Response().StatusCode())
+		}
+		sugar.Info(" ")
+		return err
+	})
+
+	validate, err := util.NewValidate()
+	if err != nil {
+		sugar.With(err).Fatalln("cannot create validator")
+	}
+
+	// middlewares
+	middlewareHandler := handlers.NewMiddlewareHandler(userService, projectService, meetingService)
+
+	// /project
+	projectHandler := handlers.NewProjectHandler(projectService, userService, s3Service, sugar, validate)
+	projectGroup := app.Group("/project")
+	routes.ProjectRoutes(projectGroup, projectHandler, middlewareHandler)
+
+	// /meetings
+	meetingHandler := handlers.NewMeetingHandler(meetingService, projectService, userService, sugar, validate)
+	meetingGroup := projectGroup.Group("/:project_id/meeting")
+	routes.MeetingRoutes(meetingGroup, meetingHandler, middlewareHandler)
+
+	// /topics
+	topicHandler := handlers.NewTopicHandler(topicService, meetingService, projectService, userService, sugar, validate)
+	topicGroup := meetingGroup.Group("/:meeting_id/topic")
+	routes.TopicRoutes(topicGroup, topicHandler, middlewareHandler)
+
+	// /comment
+	commentHandler := handlers.NewCommentHandler(
+		commentService,
+		meetingService,
+		topicService,
+		actionService,
+		projectService,
+		userService,
+		sugar,
+		validate,
+	)
+	commentGroup := projectGroup.Group("/:project_id/comment")
+	routes.CommentRoutes(commentGroup, commentHandler)
+
+	// /user
+	userHandler := handlers.NewUserHandler(userService, projectService, meetingService, topicService, actionService, sugar, validate)
+	userGroup := app.Group("/user")
+	routes.UserRoutes(userGroup, userHandler)
+
+	// /action
+	actionHandler := handlers.NewActionHandler(actionService, topicService, meetingService, userService, sugar, validate)
+	actionGroup := projectGroup.Group("/:project_id/action")
+	routes.ActionRoutes(actionGroup, actionHandler, middlewareHandler)
+
+	// /tag
+	tagHandler := handlers.NewTagHandler(projectService, sugar, validate)
+	tagGroup := projectGroup.Group("/:project_id/tag")
+	routes.TagRoutes(tagGroup, tagHandler)
+
+	// /priority
+	priorityHandler := handlers.NewPriorityHandler(projectService, sugar, validate)
+	priorityGroup := projectGroup.Group("/:project_id/priority")
+	routes.PriorityRoutes(priorityGroup, priorityHandler)
+
+	// start web server
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			sugar.With(err).Fatalln("cannot listen on :8080")
+		}
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	_ = <-c
+	sugar.Infoln("shutting down web-server")
+	_ = app.Shutdown()
 }
